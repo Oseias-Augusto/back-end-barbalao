772b3bd8215f2ce3e939801e9a2b08f4e14807f5
Zéias <oseias.augusto@aluno.ifsp.edu.br>
2025-11-04 21:23:39 -0300

    Update database connection string in apiProd.py

--- apiProd.py
+++ apiProd.py
@@ -13,7 +13,7 @@ CORS(app, origins=[
 
 def get_conn():
     conn = psycopg2.connect(
-            host="dpg-d42kp3i4d50c739qr750-a.render.com",
+            host="postgresql://root:DdDLJr8BYykOf9hJL9TWXP2eDsF2A8S6@dpg-d42kp3i4d50c739qr750-a.oregon-postgres.render.com/banco_barbalao",
             port="5432",
             database="banco_barbalao",
             user="root",
