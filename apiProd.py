79f31e1333ac420492b93255d880a6c872e89e21
Zéias <oseias.augusto@aluno.ifsp.edu.br>
2025-11-05 21:25:38 -0300

    Add new CORS origins to apiProd.py

--- apiProd.py
+++ apiProd.py
@@ -13,7 +13,8 @@ CORS(app, resources={r"/api/*": {
     "origins": [
         "https://barbalao.vercel.app",
         "https://supreme-carnival-x5xvwq7494qxh6r7j-5173.app.github.dev",
-        "https://dark-sorcery-q76pqgjx9r6q2xqrj-5175.app.github.dev"
+        "https://dark-sorcery-q76pqgjx9r6q2xqrj-5175.app.github.dev",
+        "https://curly-sniffle-7vwp946xrg9x3w546-5173.app.github.dev"
     ],
     "methods": ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     "allow_headers": ["Content-Type", "Authorization"]
