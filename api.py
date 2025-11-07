6cc01d4d8ecc5f91c3111fbf24f1957ccd61e27d
Peziiim <eduardoorx@gmail.com>
2025-11-07 20:33:37 -0300

    Update api.py

--- api.py
+++ api.py
@@ -28,7 +28,7 @@ def api_server():
             nome = data.get('nome')
             senha = data.get('senha')
 
-            conn = get_conn
+            conn = get_conn()
             cursor = conn.cursor()
 
             cursor.execute('SELECT * FROM users WHERE nome = ?', (nome,))
