77ebd020bb0130447f8b77d72ced6701fdd61be1
Peziiim <eduardoorx@gmail.com>
2025-11-08 19:19:22 +0000

    delete api

--- apiProd.py
+++ apiProd.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from encrypt import verify_password
 from flask_cors import CORS
 import psycopg2
 import os
@@ -38,6 +39,41 @@ def get_conn():
 #     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE, OPTIONS"
 #     response.headers["Access-Control-Allow-Origin"] = "*"
 #     return response
+
+#login
+@app.route('/api/login/', methods=['POST'])
+def api_server():
+        if request.method == 'OPTIONS':
+            return jsonify({"message": "CORS preflight OK"}), 200
+        data = request.get_json()
+        try:
+
+            if data == None:
+                return jsonify({
+                "message": "JSON inválido ou ausente na requisição"
+            }), 400
+
+            nome = data.get('nome')
+            senha = data.get('senha')
+
+            conn = get_conn()
+            cursor = conn.cursor()
+
+            cursor.execute('SELECT * FROM users WHERE nome = %s', (nome,))
+            usuario = cursor.fetchone()
+            if usuario:
+                if verify_password(usuario[1], senha):
+                    return jsonify({"message": "OK"}), 200
+                else:
+                    return jsonify({"message": "Usuário ou senha incorretos"}), 401
+            else:
+                return jsonify({"message": "Usuário não encontrado"}), 404
+
+        except TypeError as e:
+             print(f"Erro usuário não encontrado: {e}")
+             cursor.close()
+             conn.close()    
+        return jsonify({"route": "/login", "status": 500})
 
 
 # Cria Prod.
